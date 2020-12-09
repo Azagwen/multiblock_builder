53b5cc0dcdfcb7bad0529bdc401e715c3e376b3a
Azagwen <30528331+Azagwen@users.noreply.github.com>
2020-12-09 10:08:58 +0100

    GUI "model" tab functional the GUI still doesn't trigger any encoder but is partly functional.

--- app.py
+++ app.py
@@ -12,39 +12,43 @@ def quit_check(input_text: str):
         quit()
 
 
-while True:
+def console_generator():
+    while True:
 
-    namespace = input("Namespace: ")
-    quit_check(namespace)
-    block_name = input("Block Name: ")
-    quit_check(block_name)
-    model_path = input("Model Path: ")
-    quit_check(model_path)
-    van_tex = input("Use vanilla Texture ? (True/False): ")
-    quit_check(van_tex)
-    block_type = input("""Block Type :
-0 - full block
-1 - stairs
-2 - slab
-3 - wall
->""")
-    quit_check(block_type)
-    double_slab_path = input("Double Slab Model Path (used for Type 2): ")
-    quit_check(double_slab_path)
+        namespace = input("Namespace: ")
+        quit_check(namespace)
+        block_name = input("Block Name: ")
+        quit_check(block_name)
+        model_path = input("Model Path: ")
+        quit_check(model_path)
+        van_tex = input("Use vanilla Texture ? (True/False): ")
+        quit_check(van_tex)
+        block_type = input("""Block Type :
+    0 - full block
+    1 - stairs
+    2 - slab
+    3 - wall
+    >""")
+        quit_check(block_type)
+        double_slab_path = input("Double Slab Model Path (used for Type 2): ")
+        quit_check(double_slab_path)
 
-    if van_tex == "False":
-        is_texture_vanilla = False
-    else:
-        is_texture_vanilla = True
+        if van_tex == "False":
+            is_texture_vanilla = False
+        else:
+            is_texture_vanilla = True
 
-    create_output_dirs(model_path)
-    create_colored_block_models(
-        namespace=namespace,
-        block_name=block_name,
-        model_path=model_path,
-        is_tex_vanilla=is_texture_vanilla,
-        block_type=int(block_type),
-        double_slab_path=double_slab_path
-    )
+        create_output_dirs(model_path)
+        create_colored_block_models(
+            namespace=namespace,
+            block_name=block_name,
+            model_path=model_path,
+            is_tex_vanilla=is_texture_vanilla,
+            block_type=int(block_type),
+            double_slab_path=double_slab_path
+        )
 
-    print("")
+        print("")
+
+
+console_generator()
--- gui_PyQt.py
+++ gui_PyQt.py
@@ -6,93 +6,59 @@ from PyQt5.QtWidgets import (
     QLabel,
     QPushButton,
     QLineEdit,
-    QSizePolicy,
     QFrame,
     QTabWidget,
-    QComboBox
+    QComboBox,
+    QSpacerItem
 )
 from PyQt5.QtCore import (
     QRect,
     QMargins,
     Qt,
+    QObject
+
 )
 from PyQt5.QtGui import (
-    QPalette,
+    QPalette
 )
 import sys
 
+
 h_space = 15
 v_space = 15
+window_width = 400
+window_height = 400
+tlh = 120
 
-class SettingsTabWidget:
-
-    def __init__(self, parent_widget: QWidget):
-        self.parent_widget = parent_widget
-        self.red = QPalette()
-        self.red.setColor(QPalette.Window, Qt.red)
-        self.green = QPalette()
-        self.green.setColor(QPalette.Window, Qt.green)
-        self.blue = QPalette()
-        self.blue.setColor(QPalette.Window, Qt.blue)
-        self.cyan = QPalette()
-        self.cyan.setColor(QPalette.Window, Qt.cyan)
-
-    def settings_tab_widget(self):
-        tw = QTabWidget(self.parent_widget)
-
-        test_widget = QWidget()
-        test_widget.setPalette(self.green)
-        test_widget.setAutoFillBackground(True)
-
-        tw.addTab(self.models_widget(), "Models")
-        tw.addTab(self.recipes_widget(), "Recipes")
-        tw.addTab(self.loot_tables_widget(), "Loot Tables")
-
-        return tw
-
-    def create_widget(self, bg_color: Qt):
-        w = QWidget()
-        w.setPalette(bg_color)
-        w.setAutoFillBackground(True)
-
-        return w
-
-    def create_layout(self, parent_widget: QWidget):
-        l = QGridLayout(parent_widget)
-        l.setHorizontalSpacing(h_space)
-        l.setVerticalSpacing(v_space)
-        l.setContentsMargins(QMargins(15, 15, 15, 15))
-
-        return l
-
-    def models_widget(self):
-        models = self.create_widget(self.red)
-
-        cb = QComboBox(models)
-        cb.addItem("Full Block")
-        cb.addItem("Stairs")
-        cb.addItem("Slab")
-        cb.addItem("Wall")
-        cb.setEditable(False)
-
-        layout_models = self.create_layout(models)
-        layout_models.addWidget(cb)
-
-        return models
-
-    def recipes_widget(self):
-        recipes = self.create_widget(self.blue)
-        layout_recipes = self.create_layout(recipes)
-
-        return recipes
-
-    def loot_tables_widget(self):
-        loot_tables = self.create_widget(self.cyan)
-        layout_loot_tables = self.create_layout(loot_tables)
-
-        return loot_tables
+red = QPalette()
+red.setColor(QPalette.Window, Qt.red)
+yellow = QPalette()
+yellow.setColor(QPalette.Window, Qt.yellow)
+green = QPalette()
+green.setColor(QPalette.Window, Qt.green)
+cyan = QPalette()
+cyan.setColor(QPalette.Window, Qt.cyan)
+blue = QPalette()
+blue.setColor(QPalette.Window, Qt.blue)
 
 
+def make_line(parent_widget: QWidget):
+    line = QFrame(parent_widget)
+    line.setFrameShape(QFrame.HLine)
+    return line
+
+
+def make_line_edit(ph_text: str, parent_widget: QWidget):
+    le = QLineEdit(parent_widget)
+    le.setPlaceholderText(ph_text)
+    le.setMaxLength(40)
+    return le
+
+
+def make_push_button(ph_text: str, parent_widget: QWidget):
+    pb = QPushButton(parent_widget)
+    pb.setText(ph_text)
+    return pb
 
 class Gui(QMainWindow):
 
@@ -100,23 +66,7 @@ class Gui(QMainWindow):
         super(Gui, self).__init__()
         self.init_gui()
 
-    def make_line(self, parent_widget: QWidget):
-        line = QFrame(parent_widget)
-        line.setFrameShape(QFrame.HLine)
-        return line
-
-    def make_line_edit(self, ph_text: str, parent_widget: QWidget):
-        le = QLineEdit(parent_widget)
-        le.setPlaceholderText(ph_text)
-        le.setMaxLength(40)
-        return le
-
-    def make_push_button(self, ph_text: str, parent_widget: QWidget):
-        pb = QPushButton(parent_widget)
-        pb.setText(ph_text)
-        return pb
-
-    def make_grid_layout_widget(self, margin: QMargins, bg_color: Qt, offset_x: int, offset_y: int, width: int, height: int):
+    def create_grid_layout_widget(self, margin: QMargins, bg_color: Qt, offset_x: int, offset_y: int, width: int, height: int):
         layout_widget = QWidget(self)
         layout_widget.setGeometry(offset_x, offset_y, width, height)
         layout_widget.setPalette(bg_color)
@@ -129,8 +79,85 @@ class Gui(QMainWindow):
 
         return layout, layout_widget
 
+    ######################################################
+    ##             MODEL SETTINGS TAB-WIDGET            ##
+    ######################################################
+
+    def settings_tab_widget(self, parent_widget: QWidget):
+        tw = QTabWidget(parent_widget)
+
+        test_widget = QWidget()
+        test_widget.setPalette(green)
+        test_widget.setAutoFillBackground(True)
+
+        tw.addTab(self.models_widget(), "Models")
+        tw.addTab(self.recipes_widget(), "Recipes")
+        tw.addTab(self.loot_tables_widget(), "Loot Tables")
+
+        return tw
+
+    def create_simple_widget(self, bg_color: Qt):
+        w = QWidget()
+        w.setPalette(bg_color)
+        w.setAutoFillBackground(True)
+
+        return w
+
+    def create_simple_layout(self, parent_widget: QWidget, hs: int, vs: int):
+        l = QGridLayout(parent_widget)
+        l.setHorizontalSpacing(hs)
+        l.setVerticalSpacing(vs)
+        l.setContentsMargins(QMargins(15, 15, 15, 15))
+
+        return l
+
+    def models_widget(self):
+        models = self.create_simple_widget(red)
+
+        self.texture_top_le = make_line_edit("Texture Top: ", models)
+        self.texture_sides_le = make_line_edit("Texture All: ", models)
+        self.texture_bottom_le = make_line_edit("Texture Bottom: ", models)
+
+        self.texture_top_le.setVisible(False)
+        self.texture_bottom_le.setVisible(False)
+
+        self.model_type_cb = QComboBox(models)
+        self.model_type_cb.addItem("Full Block")
+        self.model_type_cb.addItem("Stairs    ")
+        self.model_type_cb.addItem("Slab      ")
+        self.model_type_cb.addItem("Wall      ")
+        self.model_type_cb.setEditable(False)
+        self.model_type_cb.currentIndexChanged.connect(self.on_model_type_changed)
+
+        spacer = QSpacerItem(0, 10)
+
+        layout_models = self.create_simple_layout(models, 5, 5)
+        layout_models.addWidget(self.model_type_cb, 0, 1, 1, 3)
+        layout_models.addItem(spacer)
+        layout_models.addWidget(self.texture_top_le, 1, 1)
+        layout_models.addWidget(self.texture_sides_le, 1, 2)
+        layout_models.addWidget(self.texture_bottom_le, 1, 3)
+
+        return models
+
+    def recipes_widget(self):
+        recipes = self.create_simple_widget(blue)
+        layout_recipes = self.create_simple_layout(recipes, 5, 5)
+
+        return recipes
+
+    def loot_tables_widget(self):
+        loot_tables = self.create_simple_widget(cyan)
+        layout_loot_tables = self.create_simple_layout(loot_tables, 5, 5)
+
+        return loot_tables
+
+    ###################################################
+    ##             "GENERATE" PUSH BUTTON            ##
+    ###################################################
+
     def generate_button(self, parent_widget: QWidget):
-        generate_pb = self.make_push_button("Generate", parent_widget)
+        generate_pb = make_push_button("Generate", parent_widget)
         generate_pb.clicked.connect(self.clicked)
         return generate_pb
 
@@ -138,20 +165,11 @@ class Gui(QMainWindow):
         # Qrect(pos_x, pos_y, size_x, size_y)
         # QMargins(left, top, right, bottom)
 
-        green = QPalette()
-        green.setColor(QPalette.Window, Qt.green)
-        yellow = QPalette()
-        yellow.setColor(QPalette.Window, Qt.yellow)
-
-        window_width = 500
-        window_height = 600
-        tlh = 150
-
         self.setFixedSize(window_width, window_height)
         self.setWindowTitle("Window UwU")
 
-        self.top_layout = self.make_grid_layout_widget(
-            margin=QMargins(15, 15, 15, 0),
+        self.top_layout = self.create_grid_layout_widget(
+            margin=QMargins(15, 15, 15, 15),
             bg_color=green,
             offset_x=0,
             offset_y=0,
@@ -159,7 +177,7 @@ class Gui(QMainWindow):
             height=tlh
         )
 
-        self.bottom_layout = self.make_grid_layout_widget(
+        self.bottom_layout = self.create_grid_layout_widget(
             margin=QMargins(15, 0, 15, 15),
             bg_color=yellow,
             offset_x=0,
@@ -168,15 +186,39 @@ class Gui(QMainWindow):
             height=window_height - tlh
         )
 
-        self.top_layout[0].addWidget(self.make_line_edit("Namespace: ", self.top_layout[1]))
-        self.top_layout[0].addWidget(self.make_line_edit("Block Name: ", self.top_layout[1]))
-        self.top_layout[0].addWidget(self.make_line(self.top_layout[1]))
-        self.bottom_layout[0].addWidget(SettingsTabWidget(self.bottom_layout[1]).settings_tab_widget())
-        self.bottom_layout[0].addWidget(self.make_line(self.bottom_layout[1]))
+        self.top_layout[0].addWidget(make_line_edit("Namespace: ", self.top_layout[1]))
+        self.top_layout[0].addWidget(make_line_edit("Block Name: ", self.top_layout[1]))
+        self.top_layout[0].addWidget(make_line(self.top_layout[1]))
+        self.bottom_layout[0].addWidget(self.settings_tab_widget(self.bottom_layout[1]))
+        self.bottom_layout[0].addWidget(make_line(self.bottom_layout[1]))
         self.bottom_layout[0].addWidget(self.generate_button(self.bottom_layout[1]))
 
     def clicked(self):
         print("UwU")
+
+    def on_model_type_changed(self):
+        index = self.model_type_cb.currentIndex()
+        type = self.model_type_cb.currentText()
+        print(f"[MODEL TYPE] current type: {type} || at index {index}")
+        if index == 0:
+            self.texture_sides_le.setPlaceholderText("Texture All:")
+            self.texture_top_le.setVisible(False)
+            self.texture_bottom_le.setVisible(False)
+
+        elif index == 1:
+            self.texture_sides_le.setPlaceholderText("Texture Sides:")
+            self.texture_top_le.setVisible(True)
+            self.texture_bottom_le.setVisible(True)
+
+        elif index == 2:
+            self.texture_sides_le.setPlaceholderText("Texture Sides:")
+            self.texture_top_le.setVisible(True)
+            self.texture_bottom_le.setVisible(True)
+
+        elif index == 3:
+            self.texture_sides_le.setPlaceholderText("Texture All:")
+            self.texture_top_le.setVisible(False)
+            self.texture_bottom_le.setVisible(False)
 
 
 def window():
